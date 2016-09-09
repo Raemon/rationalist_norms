168f15603e3d630bce7ee77921a0978aafcbdce4
Raymond Arnold <rarnold@spotify.com>
2016-09-08 20:14:51 -0400

    some basic habits

---
+++ root.py
@@ -0,0 +1,16 @@
+Entry level habits to consider cultivating:
+Practice noticing emotional states that seem important (so you can later chain triggers off them)
+
+
+Mid level habits:
+
+Precondition: ability to notice when you are feeling defensive or angry
+Trigger: Notice that you are defensive or angry, and that you are attempting rational discourse
+Action: Take deep breaths and then either tap out of the conversation or something
+
+Trigger: You ask a group a question, where it matters what the collective intelligence of the answer is
+Action: Ask everyone to wait 12 seconds before answering
+Reason: • Reduces Asch conformity/priming
+        • Reduces cached thoughts
+        • Allows slower thinkers to practice thinking
+
